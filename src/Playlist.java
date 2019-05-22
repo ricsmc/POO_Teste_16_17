ca034732a5bc0b9c5aa36ad2723d9456d4631bf3
ricsmc <39051605+ricsmc@users.noreply.github.com>
2019-05-22 16:12:22 +0100

    ex 2

---
+++ src/Playlist.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Playlist {
+    private String nome;
+    private Map<String, List<Faixa>> musicas;
+
+    public Playlist(String nome, Map<String, List<Faixa>> musicas) {
+        this.nome = nome;
+        this.musicas = new HashMap<>();
+        for(Map.Entry<String, List<Faixa>> i : musicas.entrySet()){
+            List<Faixa> fx = new ArrayList<>();
+            for(Faixa j : i.getValue()){
+                fx.add(j);
+            }
+            this.musicas.put(i.getKey(), fx);
+        }
+    }
+
+    public List<Faixa> getFaixas (String autor) { //throws AutorInexistenteException
+        return this.musicas.entrySet().stream().filter(l -> l.getKey().equals(autor)).
+                flatMap(x-> x.getValue().stream()).collect(Collectors.toList());
+
+    }
+
+    public double tempoTotal (String autor){ //throws AutorInexistenteException
+        return this.getFaixas(autor).stream().map(x -> x.getNumeroDeVezesTocada() * x.getDuracao()).
+                mapToDouble(Double::doubleValue).sum();
+    }
+
+
+}
